d0244ec3f03c64b862fd2c12ea9b6dea1dd07f83
JulissaDantes <julissadcj@gmail.com>
2025-05-14 14:52:48 -0400

    add week 1 directory

---
+++ week_1/target/debug/deps/week_1-057ef7318b6fa8b3.d
@@ -0,0 +1,5 @@
+/Users/julissadantes-castillo/Documents/rareskills-rust-bootcamp/week_1/target/debug/deps/libweek_1-057ef7318b6fa8b3.rmeta: src/main.rs
+
+/Users/julissadantes-castillo/Documents/rareskills-rust-bootcamp/week_1/target/debug/deps/week_1-057ef7318b6fa8b3.d: src/main.rs
+
+src/main.rs:
---
+++ week_1/target/debug/deps/week_1-3959e02066385f7f.d
@@ -0,0 +1,5 @@
+/Users/julissadantes-castillo/Documents/rareskills-rust-bootcamp/week_1/target/debug/deps/libweek_1-3959e02066385f7f.rmeta: src/main.rs
+
+/Users/julissadantes-castillo/Documents/rareskills-rust-bootcamp/week_1/target/debug/deps/week_1-3959e02066385f7f.d: src/main.rs
+
+src/main.rs:
